98f46d22ac35909525cc6f463068d22cdb45c23d
dehmer <horst.dehmer@gmail.com>
2026-07-12 07:33:31 +0200

    strokes_plot: updated to new Stroke API.

---
+++ src/kanji_nn/plot/strokes_plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def _strokes(strokes, figsize):
+    figure = plt.figure(figsize=figsize)
+    for i, stroke in enumerate(strokes):
+        xy = stroke.xy
+        plt.plot(xy[:, 0], xy[:, 1], color='black', zorder=1)
+        plt.scatter(xy[:, 0],  xy[:, 1], marker='o', color='red', zorder=2, alpha=.3)
+        plt.scatter(xy[0, 0],  xy[0, 1], marker='o', color='green', zorder=3)
+        plt.scatter(xy[-1, 0], xy[-1, 1], marker='o', color='blue', zorder=3)
+
+    # Kanji coordinate systems usually start at the top-left (invert Y-axis)
+    ax = plt.gca()
+    ax.invert_yaxis()
+    ax.set_xlim([0, 1]) # assume fixed x values [0, 1]
+    ax.set_ylim([1, 0]) # assume fixed y values [0, 1]
+    ax.set_axis_off()
+
+    return figure
+
+
+def save(filename, strokes, figsize=(6, 6)):
+    figure = _strokes(strokes, figsize)
+    plt.savefig(filename)
+    plt.close(figure)
+
+
+def show(strokes, figsize=(6, 6)):
+    figure = _strokes(strokes, figsize)
+    plt.show()
