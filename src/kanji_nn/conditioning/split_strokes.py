0fcfa819fa43192f5c353940514cf64d277cbfc0
dehmer <horst.dehmer@gmail.com>
2026-07-12 07:24:39 +0200

    split_strokes: dropped explicit pen column.

--- src/kanji_nn/conditioning/split_strokes.py
+++ src/kanji_nn/conditioning/split_strokes.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def split_strokes(raw):
-    # drop pen status and split into strokes
-    split_indices = np.where(raw[:, 2] == 0)[0] + 1
+    """
+    Drop pen status and split into strokes.
+    Assumes pen state as last column.
+    """
+    split_indices = np.where(raw[:, -1] == 0)[0] + 1
     raw = raw[:, :-1]
     return np.split(raw, split_indices[:-1])
