ad98afb1ed4c9393f6f60d23869968c968e4014d
dehmer <horst.dehmer@gmail.com>
2026-07-12 07:35:17 +0200

    housekeeping/clean-up.

--- src/kanji_nn/conditioning/__init__.py
+++ src/kanji_nn/conditioning/__init__.py
@@ -8,6 +8,5 @@ from .rdp_budget import rdp_to_budget_flat, rdp_to_budget_weighted
 from .resample_cubic_spline import resample_cubic_spline
 from .simplify_rdp import simplify_rdp
 from .smooth_chaikin import smooth_chaikin
-from .smooth_gaussian import smooth_gaussian
 from .adaptive_sampling import sample_kanjivg_char
 from .split_strokes import split_strokes
--- src/kanji_nn/plot/__init__.py
+++ src/kanji_nn/plot/__init__.py
@@ -1,3 +1,4 @@
-from .character import save, show
 from .kinematics import Kinematics
+from .multi_channel_plot import multi_channel_plot
 from .plot_stroke_geometry import plot_stroke_geometry
+from .strokes_plot import save, show
