8908f23d1f70548f03e29360dfb0840c97937760
dehmer <horst.dehmer@gmail.com>
2026-07-16 22:34:15 +0200

    CJK_STROKE_H: improved robustness.

--- src/kanji_nn/data/find_trim_region.py
+++ src/kanji_nn/data/find_trim_region.py
@@ -6,14 +6,20 @@ default_detector = lambda stroke: stroke.clone(props = {"cuts": (0, stroke.n_poi
 
 def CJK_STROKE_H(stroke):
     ds = stroke.features["ds"]
-    # candidate_head = stroke.props["candidate_head"]
-    # candidate_tail = stroke.props["candidate_tail"]
-
     ds_max_idx = np.argmax(ds)
+
     head_mask = ds[:ds_max_idx] > 0
+    if np.all(head_mask):
+        head_cut = 0
+    else:
+        head_cut = len(head_mask) - 1 - np.argmax(~head_mask[::-1])  # inclusive
+
     tail_mask = ds[ds_max_idx:] > 0
-    head_cut = len(head_mask) - 1 - np.argmax(~head_mask[::-1]) # inclusive
-    tail_cut = np.argmin(tail_mask) + ds_max_idx + 1 # exclusive
+    if np.all(tail_mask):
+        tail_cut = stroke.n_points
+    else:
+        tail_cut = np.argmin(tail_mask) + ds_max_idx + 1 # exclusive
+
     return stroke.clone(props = {"cuts": (head_cut, tail_cut)})
 
 
